a5b328cf40d69576214bffe7fa952e74234fc958
Aakash Pandey <aakashp2000@gmail.com>
2022-01-13 17:21:05 +0530

    Create server.c

---
+++ server.c
@@ -0,0 +1,58 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#include<string.h>
+
+int main(int argc,char *argv[])
+{
+int clientSocketDescriptor,socketDescriptor;
+
+struct sockaddr_in serverAddress,clientAddress;
+socklen_t clientLength;
+
+char recvBuffer[1000],sendBuffer[1000];
+pid_t cpid;
+bzero(&serverAddress,sizeof(serverAddress));
+serverAddress.sin_family=AF_INET;
+serverAddress.sin_addr.s_addr=htonl(INADDR_ANY);
+serverAddress.sin_port=htons(5500);
+/*TCP socket is created, an Internet socket address structure is filled with
+wildcard address & server’s well known port*/
+socketDescriptor=socket(AF_INET,SOCK_STREAM,0);
+bind(socketDescriptor,(struct sockaddr*)&serverAddress,sizeof(serverAddress));
+/*Listen function specifies the maximum number of connections that kernel should queue
+for this socket*/
+listen(socketDescriptor,5);
+printf("%s\n","Server is running ...");
+/*The server to return the next completed connection from the front of the
+completed connection Queue calls it*/
+clientSocketDescriptor=accept(socketDescriptor,(struct sockaddr*)&clientAddress,&clientLength);
+cpid=fork();
+
+if(cpid==0)
+{
+while(1)
+{
+bzero(&recvBuffer,sizeof(recvBuffer));
+recv(clientSocketDescriptor,recvBuffer,sizeof(recvBuffer),0);
+printf("\nCLIENT : %s\n",recvBuffer);
+}
+}
+else
+{
+while(1)
+{
+
+bzero(&sendBuffer,sizeof(sendBuffer));
+printf("\nType a message here ...  ");
+fgets(sendBuffer,10000,stdin);
+send(clientSocketDescriptor,sendBuffer,strlen(sendBuffer)+1,0);
+printf("\nMessage sent !\n");
+}
+}
+return 0;
+}
